4f12db12698ba5a2c74972bc1b00cade87aa8a71
Adrian030304 <adrianromanenco1@gmail.com>
2024-11-14 18:44:34 +0200

    implement stuff into the delete function

--- project.py
+++ project.py
@@ -13,13 +13,19 @@ def add_person():
 
 def delete_contact(people):
     for i, person in enumerate(people):
-        print(i + 1, + "-", person["name"], "|", person["age"], "|", person["email"])
-
+        print(i + 1, "-", person["name"], "|", person["age"], "|", person["email"])
+        
+    number_to_delete = input("Enter a number to delete: ")
+    try:
+        number = int(number)
+    except:
+        print("Invalid number")
 print("Welcome to the contact management system")
 print()
-command = input("You can 'Add', 'Delete', or 'Search' and 'Q' for quit: ")
 people = []
+
 while True:
+    command = input("You can 'Add', 'Delete', or 'Search' and 'Q' for quit: ")
     if command == "add":
         person = add_person()
         people.append(person)
