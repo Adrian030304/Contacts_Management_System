6bb150cf16a88ed4e361be6291f1f82d5643748d
Adrian030304 <adrianromanenco1@gmail.com>
2024-11-14 18:18:12 +0200

    declare main variables representing personal data

---
+++ project.py
@@ -0,0 +1,16 @@
+
+names = []
+ages = []
+emails = []
+
+for i in range(3):
+    name = input("Name: ")
+    age = input("Age: ")
+    email = input("Email: ")
+
+    names.append(name)
+    ages.append(age)
+    email.append(email)
+
+
+
