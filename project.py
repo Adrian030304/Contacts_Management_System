2e3e4a3028bc4a58617aad6dbf56387ec853f652
Adrian030304 <adrianromanenco1@gmail.com>
2024-11-14 18:30:00 +0200

    if/else condition for interaction with data

--- project.py
+++ project.py
@@ -1,9 +1,5 @@
 
-people = []
-
-
-
-for i in range(3):
+def add_person():
     name = input("Name: ")
     age = input("Age: ")
     email = input("Email: ")
@@ -13,4 +9,20 @@ for i in range(3):
         "age":age,
         "email":email
     }
+    return person
+
+people = []
+
+print("Welcome to the contact management system")
+print()
+command = input("You can 'Add', 'Delete', or 'Search': ")
+
+if command == "add":
+    person = add_person()
     people.append(person)
+elif command == "delete":
+    pass
+elif command == "search":
+    pass
+else:
+    print("Invalid command")    
